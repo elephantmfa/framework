0b489f1314812a490c3ab6e4cb6c9b40af728dcd
Marisa Clardy <marisa@clardy.eu>
2019-11-15 08:13:17 -0600

    wip

---
+++ src/Elephant/Filtering/SpamAssassin.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Elephant\Filtering;
+
+use Elephant\Contracts\Mail;
+
+class SpamAssassin
+{
+    public function __construct()
+    {
+        //
+    }
+
+    public function send(Mail $email)
+    {
+        //
+    }
+}
