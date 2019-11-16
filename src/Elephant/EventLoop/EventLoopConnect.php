68aabd4e9f2f930bd5e18e071ef2508f8923ed58
Marisa Clardy <marisa@clardy.eu>
2019-11-15 19:47:36 -0600

    Some fixes for eventloop connect.

--- src/Elephant/EventLoop/EventLoopConnect.php
+++ src/Elephant/EventLoop/EventLoopConnect.php
@@ -25,24 +25,26 @@ class EventLoopConnect
     public function __invoke(ConnectionInterface $connection)
     {
         $mail = $this->app->make(Mail::class);
+        $port = explode(':', $connection->getLocalAddress())[2];
+        $mail->connection->received_port = $port;
         try {
             $mail = (new Pipeline($this->app))
                 ->send($mail)
                 ->via('filter')
                 ->through($this->filters['connect'] ?? [])
                 ->thenReturn();
-            $connection->write('220 ' . $this->app->config['app.greeting_banner']);
+            $connection->write('220 ' . $this->app->config['app.greeting_banner'] . "\r\n");
         } catch (RejectException $reject) {
             $mail->setFinalDestination('reject');
-            $connection->write((string) $reject);
+            $connection->write((string) $reject . "\r\n");
         } catch (DeferException $defer) {
             $mail->setFinalDestination('defer');
-            $connection->write((string) $defer);
+            $connection->write((string) $defer . "\r\n");
         } catch (QuarantineException $quarantine) {
             $mail->setFinalDestination('quarantine');
-            $connection->write('220 ' . $this->app->config['app.greeting_banner']);
+            $connection->write('220 ' . $this->app->config['app.greeting_banner'] . "\r\n");
         } catch (DropException $drop) {
-            $connection->write((string) $drop);
+            $connection->write((string) $drop . "\r\n");
             $connection->close();
             return null;
         }
