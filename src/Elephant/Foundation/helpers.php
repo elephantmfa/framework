28bda7aa3152149af462ceaa61bcf8ad61465d26
Marisa Clardy <marisa@clardy.eu>
2019-11-15 19:52:05 -0600

    Add some helper functions.

--- src/Elephant/Foundation/helpers.php
+++ src/Elephant/Foundation/helpers.php
@@ -1,10 +1,98 @@
 <?php
 
 use Illuminate\Support\Carbon;
+use Illuminate\Container\Container;
+
+if (!function_exists('app')) {
+    /**
+     * Get the available container instance.
+     *
+     * @param  string|null  $abstract
+     * @param  array   $parameters
+     * @return mixed|\Illuminate\Contracts\Foundation\Application
+     */
+    function app($abstract = null, array $parameters = [])
+    {
+        if (is_null($abstract)) {
+            return Container::getInstance();
+        }
+
+        return Container::getInstance()->make($abstract, $parameters);
+    }
+}
+
+if (!function_exists('config')) {
+    /**
+     * Get / set the specified configuration value.
+     *
+     * If an array is passed as the key, we will assume you want to set an array of values.
+     *
+     * @param  array|string|null  $key
+     * @param  mixed  $default
+     * @return mixed|\Illuminate\Config\Repository
+     */
+    function config($key = null, $default = null)
+    {
+        if (is_null($key)) {
+            return app('config');
+        }
+
+        if (is_array($key)) {
+            return app('config')->set($key);
+        }
+
+        return app('config')->get($key, $default);
+    }
+}
 
 if (! function_exists('info')) {
-    function info($logMessage)
+    /**
+     * Logs out the $logMessage.
+     *
+     * @param string $logMessage
+     * @return void
+     */
+    function info(string $logMessage)
     {
         echo '[' . Carbon::now() . "] $logMessage\n";
+    }
+}
+
+if (! function_exists('dd')) {
+    /**
+     * Die and var_export the data.
+     *
+     * @param mixed $dump
+     * @return void
+     */
+    function dd($dump)
+    {
+        die(var_export($dump, true) . "\n");
+    }
+}
+
+if (!function_exists('storage_path')) {
+    /**
+     * Get the path to the storage folder.
+     *
+     * @param  string  $path
+     * @return string
+     */
+    function storage_path($path = '')
+    {
+        return app('path.storage') . ($path ? DIRECTORY_SEPARATOR . $path : $path);
+    }
+}
+
+if (!function_exists('database_path')) {
+    /**
+     * Get the database path.
+     *
+     * @param  string  $path
+     * @return string
+     */
+    function database_path($path = '')
+    {
+        return app()->databasePath($path);
     }
 }
