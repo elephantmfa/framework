addee1f8a245f3cb0b34a864361402165f3787cc
Marisa Clardy <marisa@clardy.eu>
2019-11-18 13:46:08 -0600

    Add getter and setter.

--- src/Elephant/Mail/Connection.php
+++ src/Elephant/Mail/Connection.php
@@ -2,14 +2,52 @@
 
 namespace Elephant\Mail;
 
+use InvalidArgumentException;
 use Illuminate\Contracts\Support\Arrayable;
 
 class Connection implements Arrayable
 {
-    public $received_port;
-    public $protocol;
-    public $sender_ip;
-    public $sender_name;
+    protected $receivedPort;
+    protected $protocol;
+    protected $senderIp;
+    protected $senderName;
+
+    /**
+     * Get the protected params.
+     *
+     * @param string $get
+     * @return mixed
+     *
+     * @throws \InvalidArgumentException
+     */
+    public function __get(string $name)
+    {
+        if (in_array($name, ['receivedPort', 'protocol', 'senderIp', 'senderName'])) {
+            return $this->$name;
+        }
+
+        throw new InvalidArgumentException("\$name must be in ['receivedPort', 'protocol', 'senderIp', 'senderName'].");
+    }
+
+    /**
+     * Set the protected params.
+     *
+     * @param string $name
+     * @param mixed $value
+     * @return void
+     *
+     * @throws \InvalidArgumentException
+     */
+    public function __set(string $name, $value): void
+    {
+        if (in_array($name, ['receivedPort', 'protocol', 'senderIp', 'senderName',])) {
+            $this->$name = $value;
+            
+            return;
+        }
+
+        throw new InvalidArgumentException("\$name must be in ['receivedPort', 'protocol', 'senderIp', 'senderName'].");
+    }
 
     public function toArray()
     {
--- src/Elephant/Mail/Envelope.php
+++ src/Elephant/Mail/Envelope.php
@@ -3,16 +3,57 @@
 namespace Elephant\Mail;
 
 use Illuminate\Contracts\Support\Arrayable;
+use InvalidArgumentException;
 
 class Envelope implements Arrayable
 {
-    public $helo;
-    public $sender;
-    public $recipients;
+    protected $helo;
+    protected $sender;
+    protected $recipients;
 
     public function __construct()
     {
         $this->recipients = [];
+    }
+
+    /**
+     * Get the protected params.
+     *
+     * @param string $get
+     * @return mixed
+     *
+     * @throws \InvalidArgumentException
+     */
+    public function __get(string $name)
+    {
+        if (in_array($name, ['helo', 'sender', 'recipients'])) {
+            return $this->$name;
+        }
+
+        throw new InvalidArgumentException("\$name must be in ['helo', 'sender', 'recipients'].");
+    }
+
+    /**
+     * Set the protected params. Will append for recipients.
+     *
+     * @param string $name
+     * @param mixed $value
+     * @return void
+     *
+     * @throws \InvalidArgumentException
+     */
+    public function __set(string $name, $value): void
+    {
+        if (in_array($name, ['helo', 'sender', 'recipients'])) {
+            if ($name == 'recipients') {
+                $this->recipients[] = $value;
+            }
+            $this->$name = $value;
+
+            return;
+        }
+
+        throw new InvalidArgumentException("\$name must be in ['helo', 'sender', 'recipients'].");
     }
 
     public function toArray()
