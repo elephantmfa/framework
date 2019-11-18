ab9cb2b307040faa7ddc46f05edabe65a62cf095
Marisa Clardy <marisa@clardy.eu>
2019-11-18 13:47:25 -0600

    [wip] Initial BodyPart setup.

--- src/Elephant/Mail/BodyPart.php
+++ src/Elephant/Mail/BodyPart.php
@@ -2,7 +2,50 @@
 
 namespace Elephant\Mail;
 
-class BodyPart
+use Illuminate\Contracts\Support\Arrayable;
+
+class BodyPart implements Arrayable
 {
+    protected $raw;
+
+    protected $name;
+    protected $disposition; // attachment | body
+    protected $size;
+    protected $contentTransferEncoding;
+    protected $contentType;
+
+    public static function fromRaw(string $raw): BodyPart
+    {
+        $bp = new static;
+        $bp->raw = $raw;
+
+        $lines = explode("\n", $raw);
+
+        foreach ($lines as $line) {
+            //
+        }
+
+        return $bp;
+    }
+
+    public function getRaw(): string
+    {
+        return $this->raw;
+    }
     
+    public function toArray()
+    {
+        return [
+            'name' => $this->name,
+            'disposition' => $this->disposition,
+            'size' => $this->size,
+            'content_transfer_encoding' => $this->contentTransferEncoding,
+            'content_type' => $this->contentType,
+        ];
+    }
+
+    public function __toString()
+    {
+        return $this->raw;
+    }
 }
