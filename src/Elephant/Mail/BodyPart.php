5322ff0ae5b3ac6aef2fac45a7045ce4aea3597d
Marisa Clardy <marisa@clardy.eu>
2019-11-19 20:35:02 -0600

    Handle generating from raw string the body component.

--- src/Elephant/Mail/BodyPart.php
+++ src/Elephant/Mail/BodyPart.php
@@ -3,26 +3,52 @@
 namespace Elephant\Mail;
 
 use Illuminate\Contracts\Support\Arrayable;
+use Illuminate\Support\Str;
 
 class BodyPart implements Arrayable
 {
     protected $raw;
 
-    protected $name;
+    protected $filename;
     protected $disposition; // attachment | body
     protected $size;
     protected $contentTransferEncoding;
     protected $contentType;
+    protected $body = '';
 
     public static function fromRaw(string $raw): BodyPart
     {
         $bp = new static;
-        $bp->raw = $raw;
+        $bp->disposition = 'body';
 
         $lines = explode("\n", $raw);
 
+        $readingBody = false;
+        $currentLine = '';
+
         foreach ($lines as $line) {
-            //
+            if ($readingBody) {
+                $bp->body .= "$line\n";
+            }
+
+            if (Str::startsWith($line, '--')) {
+                continue;
+            }
+            $bp->raw .= "$line\n";
+
+            if (empty(trim($line))) {
+                $readingBody = true;
+                continue;
+            }
+
+            if (preg_match('/^\s+\S/', $line)) {
+                $currentLine .= $line;
+            } else {
+                if (! empty(trim($currentLine))) {
+                    static::parseData($bp, $currentLine);
+                }
+                $currentLine = $line;
+            }
         }
 
         return $bp;
@@ -47,5 +73,42 @@ class BodyPart implements Arrayable
     public function __toString()
     {
         return $this->raw;
+    }
+
+    private static function parseData(&$bp, $currentLine)
+    {
+        if (Str::startsWith(strtolower($currentLine), 'content-transfer-encoding')) {
+            [, $value] = explode(': ', $currentLine);
+
+            $bp->contentTransferEncoding = $value;
+        }
+        if (Str::startsWith(strtolower($currentLine), 'content-type')) {
+            [, $value] = explode(': ', $currentLine);
+
+            $bp->contentType = $value;
+        }
+        if (Str::startsWith(strtolower($currentLine), 'content-disposition')) {
+            [, $value] = explode(': ', $currentLine);
+
+            $parts = explode('; ', $value);
+
+            foreach ($parts as $part) {
+                if ($part == 'attachment') {
+                    $bp->disposition = $part;
+                }
+
+                if (Str::startsWith($part, 'filename=')) {
+                    [, $value] = explode('=', $part, 2);
+                    $value = trim($value, '\'"');
+                    $bp->filename = $value;
+                }
+
+                if (Str::startsWith($part, 'size=')) {
+                    [, $value] = explode('=', $part, 2);
+                    $value = trim($value, '\'"');
+                    $bp->size = (int) $value;
+                }
+            }
+        }
     }
 }
