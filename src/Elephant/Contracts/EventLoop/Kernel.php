9930f0f47deee2d7d1ffcb652daa62a14bdfb226
Marisa Clardy <marisa@clardy.eu>
2019-11-23 10:48:30 -0600

    Add eventloop kernel contract.

---
+++ src/Elephant/Contracts/EventLoop/Kernel.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Elephant\Contracts\EventLoop;
+
+interface Kernel
+{
+    /**
+     * Bootstrap the application for HTTP requests.
+     *
+     * @return void
+     */
+    public function bootstrap();
+
+    /**
+     * Runs the ReactPHP loop.
+     *
+     * @return void
+     */
+    public function handle();
+
+    /**
+     * Perform any final actions for when the loop closes.
+     *
+     * @return void
+     */
+    public function terminate();
+
+    /**
+     * Get the Elephant application instance.
+     *
+     * @return \Illuminate\Contracts\Foundation\Application
+     */
+    public function getApplication();
+}
