2a461d98f5d17c9ec80cf84ea28826a0aaaa4397
Marisa Clardy <marisa@clardy.eu>
2019-12-27 22:45:20 -0600

    A few changes:
    - Add caching of matches so that preg_match doesn't have to be constantly called for a string we've already matched on.
    - Add some static accessors.

--- src/Elephant/Helpers/Matchers/Regex.php
+++ src/Elephant/Helpers/Matchers/Regex.php
@@ -6,9 +6,14 @@ use Elephant\Helpers\Exceptions\MatcherException;
 
 class Regex
 {
+    /** @var string $pattern */
     protected $pattern = '';
 
+    /** @var int $flags */
     protected $flags = 0;
+
+    /** @var array<array<string>> $matches */
+    protected static $matches = [];
 
     /**
      * Create a new regex matcher.
@@ -37,27 +42,48 @@ class Regex
     }
 
     /**
-     * Compare a regex against $against.
+     * Compare a pattern against $against.
+     *
+     * Additionally, it will cache the matches
+     *  for this string, so that if you call self::$matches(), it will just
+     *  return without having to re-run preg_match.
      *
      * @param string $against
-     * @return bool Whether the regular expression matches.
+     * @return bool Whether the pattern matches.
      */
     public function compare(string $against): bool
     {
-        return (bool) preg_match($this->pattern, $against, $matches, $this->flags);
+        if (preg_match($this->pattern, $against, $matches, $this->flags)) {
+            if (! isset($this->matches[$against])) {
+                $this->matches[$against] = $matches;
+            }
+
+            return true;
+        }
+
+        return false;
     }
 
     /**
-     * Get the matches of a regex match against $against.
+     * Get the matches of a match against $against.
+     *
+     * This caches the matches for a string, thus if you re-scan a string
+     *  again, it no longer needs to do a preg_match against it.
      *
      * @param string $against
      * @return array<string> First item in the array is the whole match.
      * @see https://www.php.net/manual/en/function.preg-match.php
-     * @throws MatcherException If the regex doesn't match at all.
+     * @throws MatcherException If the pattern doesn't match at all.
      */
     public function matches(string $against): array
     {
+        if (isset($this->matches[$against])) {
+            return $this->matches[$against];
+        }
+
         if (preg_match($this->pattern, $against, $matches, $this->flags)) {
+            $this->matches[$against] = $matches;
+
             return $matches;
         }
 
@@ -65,11 +91,11 @@ class Regex
     }
 
     /**
-     * Check to see if regex matches anything in array.
+     * Check to see if matches anything in array.
      *
      * @param iterable<string|iterable> $against Can contain strings and compare will
      *  be called, or more arrays and this function will be called recursively.
-     * @return bool Whether or not the regex matched anything in the array.
+     * @return bool Whether or not the pattern matched anything in the array.
      */
     public function in(iterable $against): bool
     {
@@ -89,13 +115,13 @@ class Regex
     }
 
     /**
-     * Check to see if regex matches anything in array.
+     * Check to see if matches anything in array, providing the matches.
      *
      * @param iterable<string|iterable> $against Can contain strings and compare will
      *  be called, or more arrays and this function will be called recursively.
      * @return array<string> Array_merge of all the matches in the array.
      * @see https://www.php.net/manual/en/function.preg-match.php
-     * @throws MatcherException If the regex doesn't match at all.
+     * @throws MatcherException If the pattern doesn't match at all.
      */
     public function matchesIn(iterable $against): array
     {
@@ -131,5 +157,30 @@ class Regex
     public function __toString()
     {
         return $this->pattern;
+    }
+
+
+    /**
+     * Compare a $pattern against $against.
+     *
+     * @param string $pattern
+     * @param string $against
+     * @return bool Whether the pattern matches.
+     */
+    public static function match(string $pattern, string $against): bool
+    {
+        return (new static($pattern))->compare($against);
+    }
+
+    /**
+     * Compare a $pattern against all the strings in the iterable $against.
+     *
+     * @param string $pattern
+     * @param iterable<string|iterable> $against
+     * @return bool Whether the pattern matches.
+     */
+    public static function matchIn(string $pattern, iterable $against): bool
+    {
+        return (new static($pattern))->in($against);
     }
 }
